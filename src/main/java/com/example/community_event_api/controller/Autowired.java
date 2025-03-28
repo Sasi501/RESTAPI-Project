3e0b9ec7bf58597a4aae4853f967ec88298ea0f0
Sasi501 <727724eucd501@skcet.ac.in>
2025-03-28 09:13:45 +0530

    Delete src directory

--- src/main/java/com/example/community_event_api/controller/Autowired.java
+++
@@ -1,5 +0,0 @@
-package com.example.community_event_api.controller;
-
-public @interface Autowired {
-
-}
--- src/main/java/com/example/community_event_api/repository/GuestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.community_event_api.repository;
-
-import com.example.community_event_api.entity.Guest;  // Corrected import
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GuestRepository extends JpaRepository<Guest, Long> {  // Updated to Long
-}
